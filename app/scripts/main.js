d7e89b046208487f5b19deb011b7c299978338bc
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2013-08-21 19:03:59 -0500

    Re-engineered js patterns.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,98 +15,101 @@ require.config({
 require(['app', 'jquery', 'underscore', 'bootstrap'], function (app, $) {
     'use strict';
 
-    $('.frame-guest').each(function () {
-        $(this).click(function () { $(this).toggleClass('active'); });
-    });
+    var colors = ['#005c78', '#f39200', '#e84e0e', '#c51a1b', '#73361c'];
 
-    $.get('data/data.json', function (data) {
-        var colors = data.colors;
-        var template = _.template('<div class="info"><div class="name"><strong><%= guest.name %></strong></div><div class="pitch"><%= guest.pitch %></div><div class="country"><%= guest.country %></div><div class="social"><%= socialInner %></div></div>');
-        var social = _.template('<a class="social-link" href="<%= url %>" target="_blank"><img class="social-icon" src="images/social/<%= network %>.png" /></a>');
-        var available = _.map(data.guests, function (guest) {
-            var socialInner = '';
-            for (var network in guest.social)
-            {
-                socialInner += social({
-                    network: network,
-                    url: guest.social[network]
+    if ($('.frame-guest').size()) {
+
+        $('.frame-guest').each(function () {
+            $(this).click(function () { $(this).toggleClass('active'); });
+        });
+
+        $.get('/templates/guest.html', function (template) {
+            var guestTemplate = _.template(template);
+            $.get('/data/guests.json', function (data) {
+                var available = _.map(data, function (guest) {
+                    var inner = guestTemplate({ guest: guest });
+                    var bg = '';
+                    if (guest.bg) { bg = guest.bg; }
+                    else          { bg = colors[_.random(colors.length)]; }
+                    var align = '';
+                    if (guest.bg && guest.bg.search('left') > 0) { align = 'right'; }
+                    else                                         { align = 'left';  }
+                    return {
+                        inner: inner,
+                        bg: bg,
+                        align: align
+                    };
                 });
-            }
-            var inner = template({ guest: guest, socialInner: socialInner });
-            var bg = '';
-            if (guest.bg) { bg = guest.bg; }
-            else          { bg = colors[_.random(colors.length)]; }
-            var align = '';
-            if (guest.bg && guest.bg.search('left') > 0) { align = 'right'; }
-            else                                         { align = 'left';  }
-            return {
-                inner: inner,
-                bg: bg,
-                align: align
-            };
-        });
 
-        var renderGuest = function (guest, frame) {
-            frame.fadeOut(function () {
-                frame.attr('style',
-                    'background:' + guest.bg + ';' +
-                    'text-align:' + guest.align + ';' +
-                    'background-size:' + 'cover' + ';'
-                );
-                frame.html(guest.inner);
-                frame.fadeIn();
+                var renderGuest = function (guest, frame) {
+                    frame.fadeOut(function () {
+                        frame.attr('style',
+                            'background:' + guest.bg + ';' +
+                            'text-align:' + guest.align + ';' +
+                            'background-size:' + 'cover' + ';'
+                        );
+                        frame.html(guest.inner);
+                        frame.fadeIn();
+                    });
+                };
+
+                var onGoing = false;
+
+                var frames = _.map($('.frame-guest'), function (frame) {
+                    return {
+                        guest: null,
+                        frame: frame
+                    };
+                });
+
+                _.each(frames, function (frame) {
+                    frame.guest = available.splice(
+                        _.random(available.length - 1), 1
+                    )[0];
+                    renderGuest(frame.guest, $(frame.frame));
+
+                    window.setInterval(function () {
+                        if ($(frame.frame).hasClass('active') || onGoing) { return; }
+
+                        onGoing = true;
+                        var oldGuest = _.clone(frame.guest);
+                        frame.guest = available.splice(
+                            _.random(available.length - 1), 1
+                        )[0];
+                        renderGuest(frame.guest, $(frame.frame));
+                        available.push(oldGuest);
+                        onGoing = false;
+                    }, 1000 * _.random(10, 15));
+                });
             });
-        };
-
-        var onGoing = false;
-
-        var frames = _.map($('.frame-guest'), function (frame) {
-            return {
-                guest: null,
-                frame: frame
-            };
         });
-
-        _.each(frames, function (frame) {
-            frame.guest = available.splice(
-                _.random(available.length - 1), 1
-            )[0];
-            renderGuest(frame.guest, $(frame.frame));
-
-            window.setInterval(function () {
-                if ($(frame.frame).hasClass('active') || onGoing) { return; }
-
-                onGoing = true;
-                var oldGuest = _.clone(frame.guest);
-                frame.guest = available.splice(
-                    _.random(available.length - 1), 1
-                )[0];
-                renderGuest(frame.guest, $(frame.frame));
-                available.push(oldGuest);
-                onGoing = false;
-            }, 1000 * _.random(10, 15));
-
-        });
-
-    });
+    };
 
     // Get the sponsors
 
-    $.get('data/data.json', function (data) {
-        var sponsorInner = _.template('<a href="<%= sponsor.url %>" class="sponsor sponsor-<%= sponsor.type %>" title="<%= sponsor.name %>" rel="nofollow" target="_blank"><img src="<%= sponsor.img %>" alt="<%= sponsor.name %>"></a>');
-        _.each(_.shuffle(data.sponsors), function (sponsor) {
-            $('#sponsors').append(sponsorInner({sponsor : sponsor}));
+    if ($('#sponsors').size()) {
+        $.get('/templates/sponsor.html', function (template) {
+            var sponsorTemplate = _.template(template);
+            $.get('data/sponsors.json', function (data) {
+                _.each(_.shuffle(data), function (sponsor) {
+                    $('#sponsors').append(sponsorTemplate({sponsor : sponsor}));
+                });
+            });    
         });
-    });
+    };
 
     // Get the organizers
 
-    $.get('data/data.json', function (data) {
-        var organizerInner = _.template('<a href="<%= organizer.url %>" class="organizer organizer-<%= organizer.type %>" title="<%= organizer.name %>" rel="nofollow" target="_blank"><img src="<%= organizer.img %>" alt="<%= organizer.name %>"></a>');
-        _.each(data.organizers, function (organizer) {
-            $('#organizers').append(organizerInner({ organizer: organizer }));
+    if ($('#organizers').size()) {
+        $.get('/templates/organizer.html', function (template) {
+            var organizerTemplate = _.template(template);
+            $.get('data/organizers.json', function (data) {
+                _.each(_.shuffle(data), function (organizer) {
+                    $('#organizers').append(organizerTemplate({organizer : organizer}));
+                });
+            });    
         });
-    });
+    };
 
     // Anything else
 
